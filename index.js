67dab3f822e36d2f3108f69e85fae60910c98fbc
bengarlock <ben.garlock@gmail.com>
2021-10-10 15:45:41 -0400

    se resize works

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
                 const root = document.getElementById('root')
                 const div = document.createElement('div')
                 div.className = 'item'
+
+                const nwDiv = document.createElement('div')
+                nwDiv.className = "resizer nw"
+
+                const neDiv = document.createElement('div')
+                neDiv.className = "resizer ne"
+
+                const swDiv = document.createElement('div')
+                swDiv.className = "resizer sw"
+
+                const seDiv = document.createElement('div')
+                seDiv.className = "resizer se"
+
+                div.appendChild(nwDiv)
+                div.appendChild(neDiv)
+                div.appendChild(swDiv)
+                div.appendChild(seDiv)
+
                 root.appendChild(div)
             }
         })
@@ -16,13 +34,14 @@ document.addEventListener("DOMContentLoaded", () => {
         document.addEventListener('mousedown', e => {
             if (e.target.className === 'item') {
                 drag(e.target)
+            } else if (e.target.className.includes('resizer')) {
+                resize(e.target)
             }
         })
     }
 
 
     const drag = (item) => {
-        let el = item
         window.addEventListener('mousemove', mousemove)
         window.addEventListener('mouseup', mouseup)
         let prevX = item.clientX;
@@ -31,10 +50,10 @@ document.addEventListener("DOMContentLoaded", () => {
         function mousemove(e) {
             let newX = prevX - e.clientX;
             let newY = prevY - e.clientY;
-            const rect = el.getBoundingClientRect();
+            const rect = item.getBoundingClientRect();
 
-            el.style.left = rect.left - newX + 'px';
-            el.style.top = rect.top - newY + 'px';
+            item.style.left = rect.left - newX + 'px';
+            item.style.top = rect.top - newY + 'px';
 
             prevX = e.clientX
             prevY = e.clientY
@@ -44,6 +63,38 @@ document.addEventListener("DOMContentLoaded", () => {
                 window.removeEventListener('mousemove', mousemove)
                 window.removeEventListener('mouseup', mouseup)
             }
+    }
+
+    const resize = (item) => {
+        const box = item.parentElement
+
+        window.addEventListener('mousemove', mousemove)
+        window.addEventListener('mouseup', mouseup)
+
+        let prevX = box.clientX;
+        let prevY = box.clientY;
+
+        function mousemove(e) {
+            const rect = box.getBoundingClientRect()
+
+            if (item.classList.contains('se')) {
+                box.style.width = rect.width - (prevX - e.clientX) + 'px'
+                box.style.height = rect.height - (prevY - e.clientY) + 'px'
+            } else if (item.classList.contains('sw')) {
+                console.log("sw")
+                box.style.width = rect.width - (prevX + e.clientX) + 'px'
+                box.style.height = rect.height - (prevX + e.clientY) + 'px'
+            }
+
+            prevX = e.clientX;
+            prevY = e.clientY
+
+        }
+
+        function mouseup() {
+            window.removeEventListener('mousemove', mousemove)
+            window.removeEventListener('mouseup', mouseup)
+        }
     }
 
     onClickHandler()
